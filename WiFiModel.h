0bd0b4471d7f8b0062a95d9fa8ccca9f7c092b10
SupermanGC <517255198@qq.com>
2015-09-22 10:23:12 +0800

    test1

---
+++ WiFiModel.h
@@ -0,0 +1,41 @@
+//
+//  WiFiModel.h
+//  CES2016APP
+//
+//  Created by SHSW20150917 on 15/9/20.
+//  Copyright © 2015年 SHSW20150917. All rights reserved.
+//测试看看
+
+#import <Foundation/Foundation.h>
+
+@interface WiFiModel : NSObject
+
+@property (nonatomic, copy) NSString *wl50_passwd;
+
+@property (nonatomic, copy) NSString *wl50_channel;
+
+@property (nonatomic, copy) NSString *wl_wps;
+
+@property (nonatomic, copy) NSString *wl_mode;
+
+@property (nonatomic, copy) NSString *wl50_auth;
+
+@property (nonatomic, copy) NSString *wl50_wps;
+
+@property (nonatomic, copy) NSString *wl50_mode;
+
+@property (nonatomic, copy) NSString *wl_passwd;
+
+@property (nonatomic, copy) NSString *wl_ssid;
+
+@property (nonatomic, copy) NSString *wl_channel;
+
+@property (nonatomic, copy) NSString *wl50_ssid;
+
+@property (nonatomic, copy) NSString *wl50_radio;
+
+@property (nonatomic, copy) NSString *wl_radio;
+
+@property (nonatomic, copy) NSString *wl_auth;
+
+@end
---
+++ WiFiModel.m
@@ -0,0 +1,18 @@
+//
+//  WiFiModel.m
+//  CES2016APP
+//
+//  Created by SHSW20150917 on 15/9/20.
+//  Copyright © 2015年 SHSW20150917. All rights reserved.
+//
+
+#import "WiFiModel.h"
+
+@implementation WiFiModel
+
+- (void)setValue:(id)value forUndefinedKey:(NSString *)key
+{
+    NSString *abc=@"123";
+}
+
+@end
